70fe95039d5297d6cb323105d62213573669b61c
Yongjin081002 <yg4059540@gmail.com>
2025-07-16 03:13:05 +0900

    feat: view

--- Router.tsx
+++ Router.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './src/App'
+import BattlePage from './src/components/Battle';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
+        <Route path='/battle' element={<BattlePage />} />
       </Routes>
     </BrowserRouter>
   )

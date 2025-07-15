0e42bfc697920d4c8079361b36b1d31673137489
tpqls774 <tpqls774@dgsw.hs.kr>
2025-07-16 07:31:58 +0900

    feat: a

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Container, Card, AvatarWrapper, Avatar, Nickname, ExpBarArea, LevelRow, LevelLabel, LevelValue, ExpBar, ExpBarFill, ExpText, Skills, SkillButton, StartButton } from './style'
 import AvatarImg from '../../assets/boy.svg'

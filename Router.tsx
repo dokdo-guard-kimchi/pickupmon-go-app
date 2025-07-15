766bafe4b06c56213bab6890c69ee7cf737a3e07
tpqls774 <tpqls774@dgsw.hs.kr>
2025-07-16 05:28:14 +0900

    feat: u

--- Router.tsx
+++ Router.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from './src/components/main/index'
-import BattlePage from './src/components/Battle';
+import BattlePage from './src/components/battle';
 import Start from './src/components/start/index'
 import Login from './src/components/login/index'
 import SignUp from './src/components/signup/index'
 import Charactor from './src/components/charactor/index'
+import Ai from './src/page/ai/TrashAIPage'
 
 const Router = () => {
   return (
@@ -13,6 +14,7 @@ const Router = () => {
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/battle' element={<BattlePage />} />
+        <Route path='/camera' element={<Ai />} />
         <Route path='/start' element={<Start />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signUp' element={<SignUp />} />

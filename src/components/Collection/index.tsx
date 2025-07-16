69677396af1a4182abd3700d2a6d51464abe6f32
Yongjin081002 <yg4059540@gmail.com>
2025-07-16 11:15:46 +0900

    feat: server code

--- src/components/Collection/index.tsx
+++ src/components/Collection/index.tsx
@@ -1,8 +1,42 @@
 import * as S from './style';
 import DownBar from '../DownBar/index'
 import imageTest from '../../assets/boy.svg'; // 예시 이미지 경로
+import { useEffect,useState} from 'react';
 
 const index = () => {
+    const [info, setInfo] = useState([
+        {name:'string', image:'string'}
+    ])
+
+
+    useEffect(() => {
+        const onSubmit = async () => {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                alert('로그인이 필요합니다.');
+                window.location.href = '/login';
+                return;
+            } else{
+                try{
+                    const res = await fetch(`${process.env.REACT_APP_API_URL}/collection`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            Authorization: `Bearer ${token}`,
+                        },
+                    })
+                    if(res && res.data) {
+                        console.log("서버에서 컬렉션 목록이 왔습니다.",res.data);
+                    }
+                } catch(err){
+                    console.log("서버에서 컬렉션 목록을 가져오는 중 오류가 발생했습니다.", err);
+                }
+            }
+
+        }
+        onSubmit();
+    },[])
+
   return (
     <S.Wrap>
         <S.MainWrap>
